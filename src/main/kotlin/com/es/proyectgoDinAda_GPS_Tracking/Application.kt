67e3091cd554aded2cdc9b71f005b61236e23d25
agadrian <arroyogomezadrian@gmail.com>
2024-12-09 20:28:50 +0100

    First commit

---
+++ src/main/kotlin/com/es/proyectgoDinAda_GPS_Tracking/Application.kt
@@ -0,0 +1,11 @@
+package com.es.proyectgoDinAda_GPS_Tracking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/com/es/proyectgoDinAda_GPS_Tracking/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.es.proyectgoDinAda_GPS_Tracking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
